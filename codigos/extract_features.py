3bf7fb7d7990417ab127154d28a9e25185a05528
andresmejiafl <andresmejiafl@gmail.com>
2023-06-07 23:51:09 -0500

    Actualizacion matrices confusion

--- codigos/extract_features.py
+++ codigos/extract_features.py
@@ -258,3 +258,122 @@ def features_nasnet(path):
     print('******************************************************************** ')
 
     return x_train_feature, y_train, x_test_feature, y_test
+
+
+def features_vgg16_clf(path):
+    """
+    Función para utilizar la cnn VGGNET16 como extractor de caracteristicas,
+    esta función entrega como resultado las caracteristicas y etiquetado codificado
+    para las particiones de train y validación.
+    Tiene como unico parametro de entrada la ruta con las imagenes de train y test 
+    previamente separadas.
+    """
+    print('******************************************************************** ')
+    print('Extracción de caracteristicas por medio de VGGNET16')
+    print('******************************************************************** ')
+
+    SIZE = 256
+
+    test_images  = []
+    test_labels  = [] 
+
+    for directory_path in glob.glob(path + "/*"):
+
+        label_te = directory_path.split("\\")[-1]
+
+        for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            img = cv2.resize(img, (SIZE, SIZE))
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            test_images.append(img)
+            test_labels.append(label_te)
+
+    test_images = np.array(test_images)
+    test_labels = np.array(test_labels)
+
+    print("Labels: " + str(np.unique(test_labels)))
+
+    le = preprocessing.LabelEncoder()
+    le.fit(test_labels)
+    test_labels_encoded = le.transform(test_labels)
+
+    print("Labels encoded: " + str(np.unique(test_labels_encoded)))
+
+    x_test, y_test = test_images, test_labels_encoded
+    x_test = x_test / 255
+
+    VGG_model = VGG16(weights = 'imagenet', include_top = False, input_shape = (SIZE, SIZE, 3))
+
+    for layer in VGG_model.layers:
+        layer.trainable = False
+
+    X_test_feature = VGG_model.predict(x_test)
+    X_test_features = X_test_feature.reshape(X_test_feature.shape[0], -1)
+
+    print('******************************************************************** ')
+    print('Extracción de caracteristicas por medio de VGGNET16 finalizada')
+    print('******************************************************************** ')
+
+    return X_test_features, y_test
+
+
+def features_resnet_clf(path):
+    """
+    Función para utilizar la cnn RESNET como extractor de caracteristicas,
+    esta función entrega como resultado las caracteristicas y etiquetado codificado
+    para las particiones de train y validación.
+    Tiene como unico parametro de entrada la ruta con las imagenes de train y test 
+    previamente separadas.
+    """
+    print('******************************************************************** ')
+    print('Extracción de caracteristicas por medio de RESNET')
+    print('******************************************************************** ')
+
+    resnet = models.resnet50(pretrained=True)
+    resnet.avgpool = nn.AdaptiveAvgPool2d(1)    
+    resnet = resnet.eval()
+    resnet = torch.nn.Sequential(*list(resnet.children())[:-1])
+        
+    test_images  = []
+    test_labels  = [] 
+
+    for directory_path in glob.glob(path + "/*"):
+
+        label_te = directory_path.split("\\")[-1]
+
+        for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+
+            image = Image.open(img_path).convert('RGB')
+            preprocess = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+            image = preprocess(image)
+            image = image.unsqueeze(0)
+            features = resnet(image)
+            features = features.squeeze().detach().numpy()
+            test_images.append(features)
+            test_labels.append(label_te)
+    
+    test_images = np.array(test_images)
+    test_labels = np.array(test_labels)
+
+    print("Labels: " + str(np.unique(test_labels)))
+
+    le = preprocessing.LabelEncoder()
+    le.fit(test_labels)
+    test_labels_encoded = le.transform(test_labels)
+
+    print("Labels encoded: " + str(np.unique(test_labels_encoded)))
+
+    X_test_features = test_images
+    y_test = test_labels_encoded
+
+    print('******************************************************************** ')
+    print('Extracción de caracteristicas por medio de RESNET finalizada')
+    print('******************************************************************** ')
+
+    return X_test_features, y_test
